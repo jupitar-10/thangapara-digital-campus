8eef49471450c81f6f41f52fcab5de372909a5d8
jupitar <jupitar921@gmail.com>
2026-03-19 22:30:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+ base: "/thangapara-digital-campus/",
   server: {
     host: "::",
     port: 8080,
